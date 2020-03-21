184b57a46ef0385763591d1e94d809d4483930f9
Martin <martin@Martins-MBP-2.home>
2020-03-21 18:35:00 +0100

    interest rate from DB

---
+++ dbMocks/RATESMOCK.js
@@ -0,0 +1,517 @@
+var RATESMOCK = [
+    { 
+        investRange: {
+        min: 1000,
+        max: 3999
+        },
+        rates: [
+            {
+                tenor: {
+                    min: 0,
+                    max: 24
+                },
+                rate: 3.97
+            },
+            {
+                tenor: {
+                    min: 25,
+                    max: 35
+                },
+                rate: 4
+            },
+            {
+                tenor: {
+                    min: 36,
+                    max: 47
+                },
+                rate: 4.05
+            },
+            {
+                tenor: {
+                    min: 48,
+                    max: 59
+                },
+                rate: 4.1
+            },
+            {
+                tenor: {
+                    min: 60,
+                    max: 71
+                },
+                rate: 4.2
+            },
+            {
+                tenor: {
+                    min: 72,
+                    max: 84
+                },
+                rate: 4.35
+            },
+            {
+                tenor: {
+                    min: 84,
+                    max: 95
+                },
+                rate: 4.45
+            },
+        ]
+    },
+    { 
+        investRange: {
+        min: 4000,
+        max: 6999
+        },
+        rates: [
+            {
+                tenor: {
+                    min: 0,
+                    max: 24
+                },
+                rate: 3.97
+            },
+            {
+                tenor: {
+                    min: 25,
+                    max: 35
+                },
+                rate: 4
+            },
+            {
+                tenor: {
+                    min: 36,
+                    max: 47
+                },
+                rate: 4.05
+            },
+            {
+                tenor: {
+                    min: 48,
+                    max: 59
+                },
+                rate: 4.1
+            },
+            {
+                tenor: {
+                    min: 60,
+                    max: 71
+                },
+                rate: 4.2
+            },
+            {
+                tenor: {
+                    min: 72,
+                    max: 84
+                },
+                rate: 4.35
+            },
+            {
+                tenor: {
+                    min: 84,
+                    max: 95
+                },
+                rate: 4.45
+            },
+        ]
+    },
+    { 
+        investRange: {
+        min: 7000,
+        max: 14999
+        },
+        rates: [
+            {
+                tenor: {
+                    min: 0,
+                    max: 24
+                },
+                rate: 3.97
+            },
+            {
+                tenor: {
+                    min: 25,
+                    max: 35
+                },
+                rate: 4
+            },
+            {
+                tenor: {
+                    min: 36,
+                    max: 47
+                },
+                rate: 4.05
+            },
+            {
+                tenor: {
+                    min: 48,
+                    max: 59
+                },
+                rate: 4.1
+            },
+            {
+                tenor: {
+                    min: 60,
+                    max: 71
+                },
+                rate: 4.2
+            },
+            {
+                tenor: {
+                    min: 72,
+                    max: 84
+                },
+                rate: 4.35
+            },
+            {
+                tenor: {
+                    min: 84,
+                    max: 95
+                },
+                rate: 4.45
+            },
+        ]
+    },
+    { 
+        investRange: {
+        min: 15000,
+        max: 29999
+        },
+        rates: [
+            {
+                tenor: {
+                    min: 0,
+                    max: 24
+                },
+                rate: 3.97
+            },
+            {
+                tenor: {
+                    min: 25,
+                    max: 35
+                },
+                rate: 4
+            },
+            {
+                tenor: {
+                    min: 36,
+                    max: 47
+                },
+                rate: 4.05
+            },
+            {
+                tenor: {
+                    min: 48,
+                    max: 59
+                },
+                rate: 4.1
+            },
+            {
+                tenor: {
+                    min: 60,
+                    max: 71
+                },
+                rate: 4.2
+            },
+            {
+                tenor: {
+                    min: 72,
+                    max: 84
+                },
+                rate: 4.35
+            },
+            {
+                tenor: {
+                    min: 84,
+                    max: 95
+                },
+                rate: 4.45
+            },
+        ]
+    },
+    { 
+        investRange: {
+        min: 30000,
+        max: 49999
+        },
+        rates: [
+            {
+                tenor: {
+                    min: 0,
+                    max: 24
+                },
+                rate: 3.6
+            },
+            {
+                tenor: {
+                    min: 25,
+                    max: 35
+                },
+                rate: 3.65
+            },
+            {
+                tenor: {
+                    min: 36,
+                    max: 47
+                },
+                rate: 3.7
+            },
+            {
+                tenor: {
+                    min: 48,
+                    max: 59
+                },
+                rate: 3.85
+            },
+            {
+                tenor: {
+                    min: 60,
+                    max: 71
+                },
+                rate: 3.95
+            },
+            {
+                tenor: {
+                    min: 72,
+                    max: 84
+                },
+                rate: 4.1
+            },
+            {
+                tenor: {
+                    min: 84,
+                    max: 95
+                },
+                rate: 4.25
+            },
+        ]
+    },
+    { 
+        investRange: {
+        min: 50000,
+        max: 149999
+        },
+        rates: [
+            {
+                tenor: {
+                    min: 0,
+                    max: 24
+                },
+                rate: 3.6
+            },
+            {
+                tenor: {
+                    min: 25,
+                    max: 35
+                },
+                rate: 3.65
+            },
+            {
+                tenor: {
+                    min: 36,
+                    max: 47
+                },
+                rate: 3.7
+            },
+            {
+                tenor: {
+                    min: 48,
+                    max: 59
+                },
+                rate: 3.85
+            },
+            {
+                tenor: {
+                    min: 60,
+                    max: 71
+                },
+                rate: 3.95
+            },
+            {
+                tenor: {
+                    min: 72,
+                    max: 84
+                },
+                rate: 4.1
+            },
+            {
+                tenor: {
+                    min: 84,
+                    max: 95
+                },
+                rate: 4.25
+            },
+        ]
+    },
+    { 
+        investRange: {
+        min: 150000,
+        max: 399999
+        },
+        rates: [
+            {
+                tenor: {
+                    min: 0,
+                    max: 24
+                },
+                rate: 3.25
+            },
+            {
+                tenor: {
+                    min: 25,
+                    max: 35
+                },
+                rate: 3.35
+            },
+            {
+                tenor: {
+                    min: 36,
+                    max: 47
+                },
+                rate: 3.4
+            },
+            {
+                tenor: {
+                    min: 48,
+                    max: 59
+                },
+                rate: 3.45
+            },
+            {
+                tenor: {
+                    min: 60,
+                    max: 71
+                },
+                rate: 3.52
+            },
+            {
+                tenor: {
+                    min: 72,
+                    max: 84
+                },
+                rate: 3.65
+            },
+            {
+                tenor: {
+                    min: 84,
+                    max: 95
+                },
+                rate: 3.85
+            },
+        ]
+    },
+    { 
+        investRange: {
+        min: 400000,
+        max: 1000000
+        },
+        rates: [
+            {
+                tenor: {
+                    min: 0,
+                    max: 24
+                },
+                rate: 3.10
+            },
+            {
+                tenor: {
+                    min: 25,
+                    max: 35
+                },
+                rate: 3.15
+            },
+            {
+                tenor: {
+                    min: 36,
+                    max: 47
+                },
+                rate: 3.2
+            },
+            {
+                tenor: {
+                    min: 48,
+                    max: 59
+                },
+                rate: 3.25
+            },
+            {
+                tenor: {
+                    min: 60,
+                    max: 71
+                },
+                rate: 3.3
+            },
+            {
+                tenor: {
+                    min: 72,
+                    max: 84
+                },
+                rate: 3.45
+            },
+            {
+                tenor: {
+                    min: 84,
+                    max: 95
+                },
+                rate: 3.6
+            },
+        ]
+    },
+    { 
+        investRange: {
+        min: 1000001,
+        max: 2500000
+        },
+        rates: [
+            {
+                tenor: {
+                    min: 0,
+                    max: 24
+                },
+                rate: 2.9
+            },
+            {
+                tenor: {
+                    min: 25,
+                    max: 35
+                },
+                rate: 2.95
+            },
+            {
+                tenor: {
+                    min: 36,
+                    max: 47
+                },
+                rate: 3
+            },
+            {
+                tenor: {
+                    min: 48,
+                    max: 59
+                },
+                rate: 3.1
+            },
+            {
+                tenor: {
+                    min: 60,
+                    max: 71
+                },
+                rate: 3.17
+            },
+            {
+                tenor: {
+                    min: 72,
+                    max: 84
+                },
+                rate: 3.3
+            },
+            {
+                tenor: {
+                    min: 84,
+                    max: 95
+                },
+                rate: 3.45
+            },
+        ]
+    },
+];
+
+module.exports = RATESMOCK;
--- kApp/kApp.js
+++ kApp/kApp.js
@@ -1,29 +1,49 @@
+var RATESMOCK = require('../dbMocks/RATESMOCK');
+
 var pmtCalc = require('./pmtCalc');
 var insCalc = require('./insCalc');
 
 var kApp = function(payload) {
+    // CREATE CALL TO RATES DB;
+    var i;
+
+    for(let j = 0; j < RATESMOCK.length; j++) {
+        let currBucket = RATESMOCK[j];
+        let currRange = currBucket.investRange;
+        if (payload.pv*1 >= currRange.min && payload.pv*1<= currRange.max) {
+            for(let y = 0; y < currBucket.rates.length; y++) {
+                let currRate = currBucket.rates[y];
+                let currTenor = currRate.tenor;
+                if (payload.n*1 >= currTenor.min && payload.n*1 <= currTenor.max) {
+                    i = currRate.rate;
+                }
+            }
+        }
+    }
+
+
     switch (payload.period) {
         case 'm':
-            payload.i = payload.i / 12;
+            i = i / 12;
             payload.n = payload.n*1;
             break;
         case 'q':
-            payload.i = payload.i / 4;
+            i = i / 4;
             payload.n = payload.n / 3;
             break;
         case 's': 
-            payload.i = payload.i / 2;
+            i = i / 2;
             payload.n = payload.n / 6;
             break;
         case 'a':
             payload.n = payload.n / 12;
             break;
         default: 
-            payload.i = payload.i / 12;
+            i = i / 12;
             break;
     }
 
-    if (payload.insurance) {
+    if (payload.insurance === 'true') {
         var insPrice = insCalc(
             payload.pv*1,
             payload.period,
@@ -33,7 +53,7 @@ var kApp = function(payload) {
 
     var result = pmtCalc(
         payload.n*1,
-        payload.i/100,
+        i/100,
         payload.pv*1,
         payload.fv*1,
         payload.type*1,
